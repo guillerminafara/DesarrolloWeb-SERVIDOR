c1c60e52423c228ffd627831929b3cbd83eb0c72
guiller <faramagui@hotmail.com>
2025-10-06 17:55:26 +0000

    pequeño avance

--- Unidad2/CalculadoraTest.php
+++ Unidad2/CalculadoraTest.php
@@ -11,30 +11,30 @@ class CalculadoraTest extends TestCase
         $cal = new Calculadora();
         $this->assertEquals(6, $cal->sumar(2, 4), "2+4 debe dar 6");
         $this->assertTrue(true, 6 === $cal->sumar(2, 4), "2+4==6 debe devolver true");
-        $this->assertEquals(5, $cal->sumar(2, 4), "2+4 debe dar 6");
+        // $this->assertEquals(5, $cal->sumar(2, 4), "2+4 debe dar 6");
 
     }
     public function testRestar()
     {
         $cal = new Calculadora();
-        $this->assertEquals(2, $cal->restar(5, 3));  
-        $this->assertTrue(-1, $cal->restar(2, 3)); 
-        $this->assertEquals(10, $cal->restar(12, 3));
+        $this->assertEquals(1, $cal->restar(5, 4), "5-4 debe dar 1");  
+        $this->assertTrue(true,-1=== $cal->restar(2, 3), "2-3 =-1 debe devolver true"); 
+        // $this->assertEquals(9, $cal->restar(10, 3));
     }
       public function testMultiplicar()
     {
         $cal = new Calculadora();
-        $this->assertEquals(6, $cal->multiplicar(2, 3));   
-        $this->assertEquals(0, $cal->multiplicar(0, 5));   
-        $this->assertEquals(20, $cal->multiplicar(2, 3));  
+        $this->assertEquals(6, $cal->multiplicar(2, 3),"2*3 debe dar 6");   
+        $this->assertTrue(true, 0, $cal->multiplicar(0, 5), );   
+        // $this->assertEquals(10, $cal->multiplicar(3, 3));  
     }
 
     public function testDividir()
     {
         $cal = new Calculadora();
         $this->assertEquals(2, $cal->dividir(6, 3));  
-        $this->assertEquals(1.5, $cal->dividir(3, 2)); 
-        $this->assertEquals(5, $cal->dividir(6, 3));   
+        $this->assertTrue(true, 1.5, $cal->dividir(3, 2)); 
+        // $this->assertEquals(5, $cal->dividir(6, 3));   
     }
 }
 ?>
