6247bdb01e2aea1a9d3bf1d168220cc313778085
guillermina <marfarsan@alu.edu.gva.es>
2025-11-03 19:55:18 +0100

    as

--- Unidad3/e10.php
+++ Unidad3/e10.php
@@ -21,7 +21,7 @@
     <br><br>
     <label>Apellido:<input type="text" name="apellido" required></label>
     <br><br>
-    <label>Edad:<input type="text" name="edad"></label>
+    <label>Edad:<input type="text" name="edad"></label><br><br>
     <label>Peso:<input type="text" name="peso" required></label>
     <br><br>
     <label>Sexo:</label>
@@ -42,16 +42,85 @@
     <br><br>
     <label><input type="radio" name="EstadoCivil" value="Viudo" required> Viudo</label>
 
+    <label>Aficiones:</label>
+    <label><input type="checkbox" name="aficiones[]" value="Cine"> Cine</label>
+    <br>
+    <label><input type="checkbox" name="aficiones[]" value="Deporte"> Deporte</label><br>
+    <label><input type="checkbox" name="aficiones[]" value="Literatura"> Literatura</label><br>
+    <label><input type="checkbox" name="aficiones[]" value="Musica"> Musica</label><br>
+    <label><input type="checkbox" name="aficiones[]" value="Comics"> Comics</label><br>
+    <label><input type="checkbox" name="aficiones[]" value="Series"> Series</label><br>
+    <label><input type="checkbox" name="aficiones[]" value="Videojuegos"> Videojuegos</label><br>
+
+
+
     <button type="submit" name="valida">Validar</button>
-    <button type="submit" name="enviar" formaction="salidaE9.php">Enviar</button>
+    <button type="hidden" name="enviar" formaction="salidaE10.php" style="display:none;">Enviar</button>
     <button type="reset">Borrar</button>
 </form>
 
 <body>
+    <?php require_once "comprobaciones.php";
+
+    if (isset($_POST["valida"]) && $_SERVER["REQUEST_METHOD"]) {
+        $nombre = $_POST["nombre"];
+        $apellido = $_POST["apellido"];
+        $edad = $_POST["edad"];
+        $peso = $_POST["peso"];
+        $bandera = comprobarEdad($edad);
+        $bandera2 = comprobarPeso($peso);
+        $bandera3 = comprobarCasillasAficiones($peso2);
+        // $bandera4= isset($_POST["sexo"]) ? $_POST["sexo"] :;
+        if ($bandera) {
+            echo "<p id='bien'>Edad correcta</p>";
+        } else {
+            echo "<p id='mal'>Edad errónea</p>";
+        }
+
+        if ($bandera2) {
+            echo "<p id='bien'>Peso correcto</p>";
+        } else {
+            echo "<p id='mal'>Peso fuera de rango</p>";
+        }
+
+    }
+
+    function validarTodo($bandera, $bandera2, $bandera3, $bandera4, $bandera5)
+    {
+        if ($bandera && $bandera2 && $bandera3 && $bandera4 && $bandera5) {
+            echo "<button type='submit' name='enviar' formaction='salidaE10.php'>Enviar</button>";
+
+        }
+
+    }
+    function comprobarPeso($peso)
+    {
+        $a = $peso > 10 && $peso < 150;
+        return is_numeric($peso) && $a ? true : false;
+    }
+    function comprobarEdad($edad)
+    {
+        return is_numeric($edad) && comprobarMinMax($edad);
+    }
+    function comprobarCasillasAficiones()
+    {
+        $bandera = false;
+        if (isset($_POST["aficiones"]) && !empty($_POST["aficiones"])) {
+            echo "<p id='bien'>Aficiones correctas</p>";
+            $bandera = true;
+        } else {
+            echo "<p id='bien'>Aficiones incorrectas</p>";
+        }
+        return $bandera;
+    }
+    ?>
+
     <div>
         <p>Fara Santeyana María Guillermina · 2do DAW</p>
 
     </div>
+
+
 </body>
 
 </html>
