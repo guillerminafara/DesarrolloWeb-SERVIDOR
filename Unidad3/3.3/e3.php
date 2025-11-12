d8819bcba8063cd099f8200caf16ea3a1384ddb9
guillermina <marfarsan@alu.edu.gva.es>
2025-11-12 17:20:59 +0100

    as

--- Unidad3/3.3/e3.php
+++ Unidad3/3.3/e3.php
@@ -5,13 +5,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        #mal {
+            color: red;
+        }
+    </style>
 </head>
 
 <body>
     <form method="POST">
         <fieldset>
             <legend>Números a calcular</legend>
-            <label> Qué operación deseas realizar? </label> <br><br>
+            <label> Qué operación deseas realizar? </label> <br>
             <select name="operacion[]" id="operacion" multiple required>
                 <option value="Sumar">Sumar</option>
                 <option value="Restar">Restar</option>
@@ -27,38 +32,118 @@
 
     <?php
     if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
         $n1 = $_POST["primerNumero"];
         $n2 = $_POST["segundoNumero"];
         $operaciones = $_POST["operacion"];
+        print $n1;
+        print (gettype($n1));
 
+        if (is_numeric($n1) && is_numeric($n2)) {
+            $cookie_name = "num1";
+            $cookie_value = $n1;
+            $cookie_expires; //= time() + (60 * 60 * 24 * 30);
+            $cookie_path = "/";
+            setcookie($cookie_name, $cookie_value, $cookie_expires, $cookie_path);
 
-        $cookie_name = "num1";
-        $cookie_value = $n1;
-        $cookie_expires = time() + (60 * 60 * 24 * 30);
-        $cookie_path = "/";
-        setcookie($cookie_name, $cookie_value, $cookie_expires, $cookie_path);
+            $cookie_name = "num2";
+            $cookie_value = $n2;
+            $cookie_expires; //time() + (60 * 60 * 24 * 30);
+            $cookie_path = "/";
+            setcookie($cookie_name, $cookie_value, $cookie_expires, $cookie_path);
 
-        $cookie_name = "num2";
-        $cookie_value = $n2;
-        $cookie_expires = time() + (60 * 60 * 24 * 30);
-        $cookie_path = "/";
-        setcookie($cookie_name, $cookie_value, $cookie_expires, $cookie_path);
+            $cookie_name = "operaciones";
+            $cookie_value = implode(",", $operaciones);
+            $cookie_expires; // = time() + (60 * 60 * 24 * 30);
+            $cookie_path = "/";
+            setcookie($cookie_name, $cookie_value, $cookie_expires, $cookie_path);
 
-        $cookie_name = "operaciones";
-        $cookie_value = $operaciones;
-        $cookie_expires = time() + (60 * 60 * 24 * 30);
-        $cookie_path = "/";
-        setcookie($cookie_name, $cookie_value, $cookie_expires, $cookie_path);
+            $n1Anterior = $_COOKIE["num1"];
+            $n2Anterior = $_COOKIE["num2"];
+            $operacioinesAnteriores = $_COOKIE["operaciones"];
+            echo "$operaciones, $operacioinesAnteriores";
 
-        $n1Anterior = $_COOKIE["num1"];
-        $n2Anterior = $_COOKIE["num2"];
-        $operacioinesAnteriores = $_COOKIE["operaciones"];
-        echo "$operaciones, $operacioinesAnteriores";
+            echo "<h2>Resultados Actuales</h2>";
+            if (isset($n1) && isset($n2) && isset($operaciones)) {
+                salidaActual($n1, $n2, $operaciones);
+            } else {
+                echo "<p>Aún no hay valores almacenados </p>";
+            }
 
-        echo "<h2>Resultados Actuales</h2>";
-        echo "<p> $n1 $n2</p>";
+            echo "<h2>Resultados Anteriores</h2>";
+            if (isset($n1Anterior) && isset($n2Anterior) && isset($operacioinesAnteriores)) {
+                salidaAnterior($n1Anterior, $n2Anterior, $operacioinesAnteriores);
+            } else {
+                echo "<p>Aún no hay valores almacenados </p>";
+            }
+        } else {
+            echo "<p id='mal'>Datos incorrectos, ingresa números</p>";
+        }
 
-        echo "<h2>Resultados Anteriores</h2>";
+    }
+    function salidaAnterior($n1Anterior, $n2Anterior, $operacioinesAnteriores)
+    {
+        echo "<p> Primer número: $n1Anterior</p>";
+        echo "<p> Segundo Número $n2Anterior </p>";
+        echo "<p> Operaciones: $operacioinesAnteriores</p>";
+
+        $operacioinesAnteriores = explode(",", $operacioinesAnteriores);
+        foreach ($operacioinesAnteriores as $a) {
+
+            switch ($a) {
+                case "Sumar":
+                    echo "<h3>sumando</h3>";
+
+                    echo sumar($n1Anterior, $n2Anterior);
+                    break;
+                case "Restar":
+                    echo "<h3>Restando</h3>";
+                    echo restar($n1Anterior, $n2Anterior);
+                    break;
+                case "Multiplicar":
+                    echo "<h3>Multiplicando</h3>";
+                    echo multiplicar($n1Anterior, $n2Anterior);
+                    break;
+                case "Dividir":
+                    echo "<h3>Dividiendo</h3>";
+                    echo dividir($n1Anterior, $n2Anterior);
+                    break;
+                default:
+                    echo "Entra al default";
+                    break;
+            }
+        }
+    }
+    function salidaActual($n1, $n2, $operaciones)
+    {
+        echo "<p> Primer número: $n1 </p>";
+        echo "<p> Segundo Número $n2 </p>";
+        echo "<p> Operaciones: $operaciones</p>";
+
+        foreach ($operaciones as $a) {
+            switch ($a) {
+                case "Sumar":
+                    echo "<h3>sumando</h3>";
+                    echo sumar($n1, $n2);
+                    break;
+                case "Restar":
+                    echo "<h3>Restando</h3>";
+                    echo restar($n1, $n2);
+                    break;
+                case "Multiplicar":
+                    echo "<h3>Multiplicando</h3>";
+
+                    echo multiplicar($n1, $n2);
+                    break;
+                case "Dividir":
+                    echo "<h3>Dividiendo</h3>";
+                    echo dividir($n1, $n2);
+                    break;
+                default:
+                    echo "Entra al default";
+                    break;
+            }
+        }
     }
     function sumar($n1, $n2)
     {
